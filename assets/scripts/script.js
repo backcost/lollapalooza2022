f43bcb6a67718e7e219cf97d74d5d19f73b7ce6c
Gabriel <gabriel_costa_ferreira@hotmail.com>
2022-04-05 19:02:00 -0300

    mapa

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -53,12 +53,12 @@ const lyricsPage = () => {
 };
 
 const mapPage = () => {
-    backgroundColor.background = '#cdeaf2';
+    backgroundColor.background = 'var(--alabaster)';
     changeMenuLinkColor(3);
 
     main.innerHTML  = 
     `<div class="container">
-        <img alt="Mapa" class="map" src="./assets/img/mapa.jpg"/>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1636.1862843212439!2d-46.69702131073765!3d-23.702166409187747!2m3!1f270!2f0!3f0!3m2!1i1024!2i768!4f35!5e1!3m2!1spt-BR!2sbr!4v1646701192274!5m2!1spt-BR!2sbr" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <img alt="Mapa" class="map" src="./assets/img/mapa.png"/>
+        <iframe class="real-map" src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7192.368936893399!2d-46.697021!3d-23.702166!3m2!1i1024!2i768!4f13.1!5e1!3m2!1spt-BR!2sbr!4v1649195485850!5m2!1spt-BR!2sbr" width="600" height="800" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>`;
 };
