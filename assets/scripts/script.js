21ea219c6bb9b8ba308f5f7903a6d2b792b200c2
Gabriel <gabriel_costa_ferreira@hotmail.com>
2022-04-05 23:30:31 -0300

    photos

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,7 +4,15 @@ let artists = [
     { name: "The Wombats", day: "sexta", photo: "https://yt3.ggpht.com/g_a0WQqXx7dhQadXUS35460TdZ8yOzDraYHocri7Z3SKSVAcLo5kvPC8gCuEB9KgU_3FDV7jXg=s176-c-k-c0x00ffffff-no-rj" },
     { name: "The Strokes", day: "sexta", photo: "https://yt3.ggpht.com/ytc/AKedOLSkS7pdVnuIZbN_qgCSSaVdfI-ZH0mlhC4Uc1mG=s176-c-k-c0x00ffffff-no-rj" },
     { name: "070 Shake", day: "sexta", photo: "https://yt3.ggpht.com/ytc/AKedOLSSUf5KE-JtfuXU0OGVk6kOmyLqfKne2wM2RdIU=s176-c-k-c0x00ffffff-no-rj" },
-    { name: "LP", day: "sexta", photo: "https://yt3.ggpht.com/5NK5TaTv4iBx7UMk_P0tiOnysKoqQbvA7RJkG6aqGpnYq3QC4aZY3XZ3KCfaMlyjn_fz38lh=s176-c-k-c0x00ffffff-no-rj" }
+    { name: "LP", day: "sexta", photo: "https://yt3.ggpht.com/5NK5TaTv4iBx7UMk_P0tiOnysKoqQbvA7RJkG6aqGpnYq3QC4aZY3XZ3KCfaMlyjn_fz38lh=s176-c-k-c0x00ffffff-no-rj"},
+    { name: "Miley Cyrus", day: "sabado", photo: "https://yt3.ggpht.com/Q3zWn-NiT2YtyY4nbFpjpgXCMB_gIJ62rkcbmO4Oiw1zvkkJqT4gr-tNgLyDHuG0j6dAg9gg=s176-c-k-c0x00ffffff-no-rj"},
+    { name: "Two Feet", day: "sabado", photo: "https://yt3.ggpht.com/qxpqEJzRhcsuHq95Ea-zqSrCMSq5_8HieIDPhQtypr4sqr66OXL9Zq2EgMXeSOs0QP634dHH=s176-c-k-c0x00ffffff-no-rj"},
+    { name: "A$AP Rocky", day: "sabado", photo: "https://yt3.ggpht.com/MGTwNIohI7lST3m4jLmObL00zOqseYmw6bKLzZYt9Ifyy23cBRU-tfBJBddgXphRfqTVz1bUGkA=s176-c-k-c0x00ffffff-no-rj"},
+    { name: "Alessia Cara", day: "sabado", photo: "https://yt3.ggpht.com/WdUuEN8nK_iw0sABEspiwnZorTfWXAzW9nx-B9m_QDLQB5wfrVYAKhbRSb0MGMPwPS3_wQLnBQ=s176-c-k-c0x00ffffff-no-rj"},
+    { name: "Foo Fighters", day: "domingo", photo: "https://yt3.ggpht.com/iCS11jvJG82IrZH-DW89Es_0dadc6oI-ZfNe009A1NyIOlXuGU2s32p8Vx-9E_rniAFNNkKtzw=s176-c-k-c0x00ffffff-no-rj"},
+    { name: "Black Pumas", day: "domingo", photo: "https://yt3.ggpht.com/ytc/AKedOLR-Mb-z3fwkl7qG95xY0QpWJZ1gIBEjaFffI2e-=s176-c-k-c0x00ffffff-no-rj"},
+    { name: "IDLES", day: "domingo", photo: "https://yt3.ggpht.com/H7udZwofN87YM8bGBWrfC348as6h6Fblj5L8rgK-smb3XG_b3YExAQtb1IY1PVsJFkwnsjWr=s176-c-k-c0x00ffffff-no-rj"},
+    { name: "The Libertines", day: "domingo", photo: "https://yt3.ggpht.com/ytc/AKedOLTtzdgJVRMQ0GpuKbaodEBM7S07-6F5JMsJzdVm=s176-c-k-c0x00ffffff-no-rj"}
 ];
 
 const scheduleDay = (day) => {
@@ -17,7 +25,7 @@ const lyricsDay = (day) => {
     for (let i = 0; i < artists.length; i++) {
         if (artists[i].day === day) {
             artistsDiv += 
-            `<div>
+            `<div class="container">
                 <img class="artist__photo" src="${artists[i].photo}" alt="">
                 <div class="artist">${artists[i].name}</div>
             </div>`;
